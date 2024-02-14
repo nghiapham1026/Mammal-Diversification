26defa15889d39c1260c5e26b6c1c69fb545756a
Nathan Pham <nghiapham102603@gmail.com>
2024-02-14 09:18:16 -0800

    Moved model_utils to /model

--- model/model_amphibian.py
+++ model/model_amphibian.py
@@ -1,3 +1,3 @@
-from utils.model_utils import plot_diversity_over_time
+from model_utils import plot_diversity_over_time
 
 plot_diversity_over_time('./data/processed/taxon/interpolated/Amphibian_Diversity_interpolated.csv', 'Amphibian Diversity Over Time')
--- model/model_reptile.py
+++ model/model_reptile.py
@@ -1,3 +1,3 @@
-from utils.model_utils import plot_diversity_over_time
+from model_utils import plot_diversity_over_time
 
 plot_diversity_over_time('./data/processed/taxon/interpolated/Reptile_Diversity_interpolated.csv', 'Reptile Diversity Over Time')
