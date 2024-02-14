efcb4593d3a2508bafd720953e17cb887afe7752
Nathan Pham <nghiapham102603@gmail.com>
2024-02-13 23:34:20 -0800

    Utility functions for models

--- src/model/model_insect.py
+++ src/model/model_insect.py
@@ -1,24 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+from utils.model_utils import plot_diversity_over_time
 
-# Assuming 'data' is your DataFrame
-#data = pd.read_csv('../../data/processed/taxon/Insect_Diversity.csv')
-data = pd.read_csv('../../data/processed/taxon/interpolated/Insect_Diversity_interpolated.csv')
-
-# Sort the data by max_ma to ensure chronological order
-data = data.sort_values(by='max_ma')
-
-# Plotting
-plt.figure(figsize=(10, 6))
-# Scatter plot for individual data points
-plt.scatter(data['max_ma'], data['n_occs'], color='blue', label='Number of Occurrences', s=20, alpha=0.5)  # Use a fixed size
-# Line plot connecting the points
-plt.plot(data['max_ma'], data['n_occs'], color='red', label='Trend')
-
-plt.xlabel('Time (Million years ago)')
-plt.ylabel('Number of Occurrences')
-plt.title('Insect Diversity Over Time')
-plt.gca().invert_xaxis()  # Invert x-axis to show recent times at the right
-plt.grid(True)
-plt.legend()
-plt.show()
+plot_diversity_over_time('../../data/processed/taxon/interpolated/Insect_Diversity_interpolated.csv', 'Insect Diversity Over Time')
