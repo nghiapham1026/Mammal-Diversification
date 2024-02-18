b031f3f7f704640278255d89fa8efa6176c960fd
Nathan Pham <nghiapham102603@gmail.com>
2024-02-17 21:27:46 -0800

    Deletion of unused modules and datasets

--- model/model_amphibian.py
+++
@@ -1,3 +0,0 @@
-from model_utils import plot_diversity_over_time
-
-plot_diversity_over_time('../data/processed/taxon/interpolated/Amphibian_Diversity_interpolated.csv', 'Amphibian Diversity Over Time')
